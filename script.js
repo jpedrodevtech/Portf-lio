b2db35b8db45607b8d2d6f18c69f6cad486c36f7
Joao Pedro Silva Araujo <joao.760011@graduacao.fsa.br>
2025-06-27 17:45:33 -0300

    Portifolio

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    new Typed(".typing", {
+      strings: ["Tecnologia","Análise e Desenvolvimento de Sistemas"],
+      typeSpeed: 60,
+      backSpeed: 100,
+      loop: true
+    });
+  });
